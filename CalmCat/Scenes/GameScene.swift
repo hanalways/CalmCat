eaa571280f05be7bd84dc77dff69c1f87cd2614b
Hana <hanalways@gmail.com>
2019-07-11 11:30:45 -0700

    circle anchored to button

--- CalmCat/Scenes/GameScene.swift
+++ CalmCat/Scenes/GameScene.swift
@@ -9,9 +9,6 @@ import SpriteKit
 import UIKit
 
 class GameScene: SKScene {
-  // adding in the cat
-  let catSprite = SKSpriteNode(imageNamed: "catSprite")
-  
   // adding in circle sprite
   let circle = SKShapeNode(circleOfRadius: 100.0)
   
@@ -20,29 +17,50 @@ class GameScene: SKScene {
   let table = SKSpriteNode(imageNamed: "table")
   let window = SKSpriteNode(imageNamed: "window")
   
+  // cat sprites
+//  var catPurr = SKSpriteNode()
+//  var catPurrFrames: [SKTexture] = []  
+  
   override func didMove(to view: SKView) {
     backgroundColor = SKColor.black
-    // cattt
-    catSprite.position = CGPoint(x: size.width * 0.4, y: size.height * 0.4)
     
     circle.position = CGPoint(x: view.frame.width * 0.7, y: view.frame.height * 0.5)
 //    circle.fillColor = UIColor.cyan
-    addChild(circle)
-//    addChild(catSprite)
+//    addChild(circle)
     
+    // background assets
     bed.zPosition = -1
     bed.position = CGPoint(x: size.width * 0.55, y: size.height * 0.2)
     bed.size = CGSize(width: size.width, height: size.height * 0.7)
     addChild(bed)
     
     table.zPosition = -1
-    table.size = CGSize(width: size.width * 0.5, height: size.height * 0.25)
-    table.position = CGPoint(x: size.width * 0.7, y: size.height * 0.8)
+    table.size = CGSize(width: size.width * 0.7, height: size.height * 0.25)
+    table.position = CGPoint(x: size.width * 0.7, y: size.height * 0.75)
     addChild(table)
     
     window.zPosition = -1
-    window.size = CGSize(width: size.width * 0.2, height: size.height * 0.17)
+    window.size = CGSize(width: size.width * 0.2, height: size.height * 0.14)
     window.position = CGPoint(x: size.width * 0.2, y: size.height * 0.9)
     addChild(window)
+    //
+
+//    buildCatPurr()
   }
+  
+//  func buildCatPurr() {
+//    let catPurrAnimatedAtlas = SKTextureAtlas(named: "catPurr")
+//    var purrFrames: [SKTexture] = []
+//    let numImages = catPurrAnimatedAtlas.textureNames.count
+//    for i in 1...numImages {
+//      let catPurrTextureName = "catPurr-\(i)"
+//      purrFrames.append(catPurrAnimatedAtlas.textureNamed(catPurrTextureName))
+//    }
+//    catPurrFrames = purrFrames
+//
+//    let firstFrameTexture = catPurrFrames[0]
+//    catPurr = SKSpriteNode(texture: firstFrameTexture)
+//    catPurr.position = CGPoint(x: frame.midX, y: frame.midY)
+//    addChild(catPurr)
+//  }
 }
--- CalmCat/ViewControllers/GameViewController.swift
+++ CalmCat/ViewControllers/GameViewController.swift
@@ -16,11 +16,13 @@ class GameViewController: UIViewController {
     
   @IBOutlet weak var initializeUserTaps: UIButton!
     
-  var seconds = 120
+  var seconds = 70
   var timer = Timer()
   var isTimerRunning = false
   var timeOfLastTap = -1.0
   var timeDifferences = [Double]()
+  var medianTapTime = 1.0
+  let pulseLayer = CAShapeLayer()
   
   func runTimer() {
     timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(GameViewController.updateTimer)), userInfo: nil, repeats: true)
@@ -31,6 +33,7 @@ class GameViewController: UIViewController {
   @objc func updateTimer() {
     if seconds < 1 {
       timer.invalidate()
+      displayTimer.isHidden = true
     } else {
       seconds -= 1
       displayTimer.text = timeString(time: TimeInterval(seconds))
@@ -40,7 +43,8 @@ class GameViewController: UIViewController {
   func timeString(time:TimeInterval) -> String {
     let minutes = Int(time) / 60 % 60
     let seconds = Int(time) % 60
-    let rString = "\(minutes):\(seconds)"
+    let rString = String(format: "\(minutes):%02d", seconds)
+//    let rString = "\(minutes):\(seconds)"
     return rString
   }
 
@@ -62,7 +66,7 @@ class GameViewController: UIViewController {
     timeOfLastTap = timeOfCurrentTap
     
     if timeDifferences.count == 10 {
-        let medianTapTime = timeDifferences.sorted(by: <)[timeDifferences.count/2]
+        medianTapTime = timeDifferences.sorted(by: <)[timeDifferences.count/2]
         print("This is the medianTapTime", medianTapTime)
     }
   }
@@ -79,6 +83,32 @@ class GameViewController: UIViewController {
     skView.presentScene(scene)
     
     displayTimer.isHidden = true
-    
+
+//    NOT WORKING - initializeUserTaps.layer.zPosition = CGFloat(integerLiteral: 1)
+    initializeUserTaps.layer.cornerRadius = initializeUserTaps.frame.size.width/2.0
+    createPulse()
+  }
+  
+  func createPulse() {
+    let circularPath = UIBezierPath(arcCenter: .zero, radius: UIScreen.main.bounds.size.width/5, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+    pulseLayer.path = circularPath.cgPath
+    pulseLayer.lineWidth = 2.0
+    pulseLayer.fillColor = UIColor.clear.cgColor
+    pulseLayer.strokeColor = UIColor.white.cgColor
+    pulseLayer.lineCap = CAShapeLayerLineCap.round
+    pulseLayer.position = CGPoint(x: initializeUserTaps.frame.size.width/2.0, y: initializeUserTaps.frame.size.height/2.0)
+    initializeUserTaps.layer.addSublayer(pulseLayer)
+    animatePulse()
+  }
+  
+  func animatePulse() {
+    let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+    scaleAnimation.duration = 5.0
+    scaleAnimation.fromValue = 0.0
+    scaleAnimation.toValue = 0.9
+    scaleAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+    scaleAnimation.repeatCount = .greatestFiniteMagnitude
+//    scaleAnimation.autoreverses = true
+    pulseLayer.add(scaleAnimation, forKey: "scale")
   }
 }
