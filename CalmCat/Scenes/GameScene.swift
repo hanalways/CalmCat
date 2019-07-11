c5e7aa4fd546bf7f90e6e473dcfe30942437393c
Hana <hanalways@gmail.com>
2019-07-11 13:40:05 -0700

    switch to UIKit background assets

--- CalmCat/Scenes/GameScene.swift
+++ CalmCat/Scenes/GameScene.swift
@@ -9,14 +9,6 @@ import SpriteKit
 import UIKit
 
 class GameScene: SKScene {
-  // adding in circle sprite
-  let circle = SKShapeNode(circleOfRadius: 100.0)
-  
-  // background assets
-  let bed = SKSpriteNode(imageNamed: "bed")
-  let table = SKSpriteNode(imageNamed: "table")
-  let window = SKSpriteNode(imageNamed: "window")
-  
   // cat sprites
 //  var catPurr = SKSpriteNode()
 //  var catPurrFrames: [SKTexture] = []  
@@ -24,27 +16,6 @@ class GameScene: SKScene {
   override func didMove(to view: SKView) {
     backgroundColor = SKColor.black
     
-    circle.position = CGPoint(x: view.frame.width * 0.7, y: view.frame.height * 0.5)
-//    circle.fillColor = UIColor.cyan
-//    addChild(circle)
-    
-    // background assets
-    bed.zPosition = -1
-    bed.position = CGPoint(x: size.width * 0.55, y: size.height * 0.2)
-    bed.size = CGSize(width: size.width, height: size.height * 0.7)
-    addChild(bed)
-    
-    table.zPosition = -1
-    table.size = CGSize(width: size.width * 0.7, height: size.height * 0.25)
-    table.position = CGPoint(x: size.width * 0.7, y: size.height * 0.75)
-    addChild(table)
-    
-    window.zPosition = -1
-    window.size = CGSize(width: size.width * 0.23, height: size.height * 0.14)
-    window.position = CGPoint(x: size.width * 0.2, y: size.height * 0.87)
-    addChild(window)
-    //
-
 //    buildCatPurr()
   }
   
