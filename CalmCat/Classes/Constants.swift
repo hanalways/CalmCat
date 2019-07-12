85cb96810cc2032635699f6dae58ca20d8d6417d
Hana <hanalways@gmail.com>
2019-07-11 18:12:25 -0700

    clean up files, add in Constants

---
+++ CalmCat/Classes/Constants.swift
@@ -0,0 +1,34 @@
+//
+//  Constants.swift
+//  CalmCat
+//
+//  Created by Hana Clements on 7/11/19.
+//  Copyright © 2019 Hana Clements. All rights reserved.
+//
+
+import UIKit
+
+struct ImageName {
+    static let Bed = "Bed"
+    static let Table = "Table"
+    static let Window = "Window"
+}
+
+struct SoundFile {
+    // this needs to be looked for!!!
+}
+
+struct Layer {
+    static let Bed: CGFloat = 0
+    static let Table: CGFloat = 0
+    static let Window: CGFloat = 0
+}
+
+struct PhysicsCategory {
+    // maybe this??
+}
+
+struct GameConfiguration {
+    // maybe this too.
+}
+
